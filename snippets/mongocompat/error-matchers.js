58f747ef9510461ed242a1c0621f66ff7ca2e359
Anna Henningsen <anna@addaleax.net>
2021-04-18 22:41:36 +0200

    Update error-matchers.js

--- snippets/mongocompat/error-matchers.js
+++ snippets/mongocompat/error-matchers.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     matches: [
       /\b(tojson|tojsononeline|tojsonObject|printjsononeline|isString|isNumber|isObject) is not defined/,
+      /\b(cd|pwd|getHostName|hostname|_rand|_isWindows|cat|getMemInfo|isInteractive|listFiles|ls|md5sumFile|mkdir|removefile) is not defined/,
       /\.tojson is not a function/,
       /(\.getTime|\.getInc|\.toStringIncomparable) is not a function/,
       /(\.ltrim|\.rtrim|\.pad) is not a function/,
@@ -10,7 +11,7 @@ module.exports = [
       /\bRegExp\.escape is not a function/,
       /\bArray\.(contains|unique|shuffle|fetchRefs|sum|avg|stdDev) is not a function/,
       /\bObject\.(extend|bsonsize|merge|keySet) is not a function/,
-      /\bObjectId\.fromDate is not a function/,
+      /\bObjectId\.fromDate is not a function/
     ],
     message: 'Are you trying to run a script written for the legacy shell? Try running `snippet install mongocompat`'
   }
